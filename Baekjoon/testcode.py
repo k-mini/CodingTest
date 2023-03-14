6be655bc9249aefe427f9680a4c04a1c537182a3
조광민 <83693386+k-mini@users.noreply.github.com>
2023-03-14 11:55:50 +0900

    Update testcode.py

--- Baekjoon/testcode.py
+++ Baekjoon/testcode.py
@@ -2,19 +2,18 @@ import sys
 
 def test():
     sys.stdin = open('input.txt', 'r')
-    
+    input = sys.stdin.readline
     #############################  테스트코드 시작
-    temp = list(map(int, input().split(' ')))
-    N = temp[0]
-    M = temp[1]
+    n,m = map(int, input().rstrip().split())
 
-    BLOCK = []
-    for i in range(0, N):
-        BLOCK.append(input().split())
+    arr= []
+    for i in range(0, n):
+        arr.append(list(map(int,input().rstrip().split())))
 
-    print(N,M)
-    print(BLOCK)
+    print(n,m)
+    print(arr)
     ##############################  테스트 코드 종료
+
     sys.stdin.close()
     
     
@@ -23,4 +22,5 @@ if __name__ == '__main__':
 
 if __name__ == 'testcode':
     print('imported testcode')
+    print('테스트시작')
     test()
