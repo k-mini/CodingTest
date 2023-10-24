adca0b5621de63a556026988948cc2a3cbba4680
조광민 <83693386+k-mini@users.noreply.github.com>
2023-10-24 22:41:13 +0900

    [level unrated] Title: 편지, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        return message.length() * 2;
+    }
+}
