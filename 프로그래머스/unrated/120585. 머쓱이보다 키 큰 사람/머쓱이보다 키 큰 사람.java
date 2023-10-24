80bd739bf0a99cfb815b1eaea25b189c0ea77f44
조광민 <83693386+k-mini@users.noreply.github.com>
2023-10-24 22:16:22 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 1.07 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int[] array, int height) {
+        return (int) Arrays.stream(array).filter(i->i>height).count();
+    }
+}
