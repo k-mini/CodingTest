7c0409ab86cf93d56e4b3e820bd8e9872db4aab3
조광민 <83693386+k-mini@users.noreply.github.com>
2023-10-26 11:45:06 +0900

    [level 1] Title: 약수의 합, Time: 2.19 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -3,6 +3,11 @@ import java.util.stream.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        return IntStream.iterate(1,i->i<=n,i->i+1).filter(i->n%i == 0).sum() ;
+        // return IntStream.iterate(1,i->i<=n,i->i+1).filter(i->n%i == 0).sum() ;
+        
+        return IntStream.iterate(1,i->i<=Math.sqrt(n),i->i+1)
+            .filter(i-> n%i ==0)
+            .map(i-> i + n/i)
+            .sum() - (Math.sqrt(n) == (int) Math.sqrt(n) ? (int) Math.sqrt(n) : 0);
     }
 }
