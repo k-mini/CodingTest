027254ba509113bbae0eaa22f4085fa1aae65d18
조광민 <83693386+k-mini@users.noreply.github.com>
2023-10-28 23:52:15 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.34 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        return array[array.length/2];
+    }
+}
