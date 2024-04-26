e8f538dc541046200fcb49ac4111eb1ce2fd08bd
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-04-26 21:48:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.71 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    static boolean answer = true;
+    
+    public boolean solution(String s) {
+        
+        int wordLength = s.length();
+        if (wordLength != 4 && wordLength != 6) {
+            return false;
+        }
+        
+        s.chars()
+            .forEach(i-> {
+            if ( (i >= 65 && i <= 90) || (i>= 97 && i <= 122) ) {
+                answer = false;
+            }       
+        });
+        
+        return answer;
+    }
+}
