b461f1725e507e42259e934e27afdfa14e8b861a
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-04-30 00:06:43 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        return new int[]{gcd(n,m), lcm(n,m)};
+    }
+    
+    // 유클리드 호제법
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+    
+    public int lcm(int a, int b) {
+        
+        return a*b / gcd(a,b);
+    }
+}
