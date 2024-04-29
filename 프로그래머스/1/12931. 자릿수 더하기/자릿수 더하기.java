dbf2d23c9ec2c3aa5f5ba4f741e3361a9bfed1d7
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-04-29 22:53:38 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.67 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,11 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        while (n > 0) {
-            answer += n %10;
-            n /= 10;
-        }
-
-        return answer;
+        return String.valueOf(n)
+            .chars()
+            .map(Character::getNumericValue)
+            .sum();
     }
 }
