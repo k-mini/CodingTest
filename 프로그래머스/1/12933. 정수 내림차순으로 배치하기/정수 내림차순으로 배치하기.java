257afd67160688185cbe0b107fbe4929b4b6c3d2
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-04-29 23:23:45 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 3.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,13 +1,16 @@
-import java.util.stream.*;
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public long solution(long n) {
-        String str = String.valueOf(n);
-        int[] array = str.chars().map(Character::getNumericValue)
-                .sorted()
-                .toArray();
-        String answer = Arrays.stream(array).mapToObj(String::valueOf)
-                            .sorted(Collections.reverseOrder()).collect(Collectors.joining());
-        return Long.valueOf(answer);
+        long answer = 0;
+
+        String result = String.valueOf(n)
+                .chars()
+                .mapToObj(i -> Integer.valueOf(Character.getNumericValue(i)))
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        return Long.valueOf(result).longValue();
     }
 }
