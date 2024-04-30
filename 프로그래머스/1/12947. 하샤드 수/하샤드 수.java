d138c221d24975f50de1e02f3e0413a14edacd50
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-04-30 23:23:17 +0900

    [level 1] Title: 하샤드 수, Time: 2.06 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,8 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        String str = String.valueOf(x);
-        int y = str.chars().map(s-> s - 48).sum();
-        return x % y == 0 ? true : false;
+        
+        int numSum = String.valueOf(x)
+            .chars()
+            .map(Character::getNumericValue)
+            .sum();
+        
+        return x % numSum == 0 ? true : false;
     }
 }
