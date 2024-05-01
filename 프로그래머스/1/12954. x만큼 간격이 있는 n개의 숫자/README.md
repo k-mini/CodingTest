deefe54afb8ad7a437384575565324960679ce40
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2024-05-01 10:15:39 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 5.91 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/README.md
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 1] x만큼 간격이 있는 n개의 숫자 - 12954 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.07 ms
+메모리: 74.4 MB, 시간: 5.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 21:43:31
+2024년 05월 01일 10:15:36
 
 ### 문제 설명
 
--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,15 +1,11 @@
 import java.util.*;
 import java.util.stream.*;
-
 class Solution {
     public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        long initValue = Long.valueOf(x);
-        int idx = 0;
-        while (idx < n){
-            answer[idx++] = initValue;
-            initValue += x;
-        }
+        long[] answer = {};
+        answer = LongStream.iterate(1,i->i<n+1,i->i+1)
+            .map(i->i*x)
+            .toArray();
         
         return answer;
     }
