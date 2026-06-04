abcac848d23ed1d5234a9c2c24b206f64dde2a9f
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-04 21:18:21 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.09 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,13 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        List<String> list = new ArrayList<>();
-        
-        for (int i=0; i < my_string.length() ;i++) {
-            list.add(my_string.substring(i));
-        }
-        
-        return list.contains(is_suffix) ? 1 : 0;
+        int answer = 0;
+        return answer;
     }
 }
