31c22a4cf8a7e0829fe0238e59f1d79727b11883
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-23 22:31:40 +0900

    [level 0] Title: 글자 지우기, Time: 3.94 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder answer = new StringBuilder();
+        List<Integer> list = Arrays.stream(indices)
+                .boxed()
+                .collect(Collectors.toList());
+
+
+        for (int i=0; i < my_string.length() ;i++) {
+
+            if (!list.contains(i)) {
+                answer.append(my_string.charAt(i));
+            }
+
+        }
+
+        return answer.toString();
+    }
+}
