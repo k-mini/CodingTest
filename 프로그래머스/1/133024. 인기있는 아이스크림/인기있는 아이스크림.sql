32098d23d025935de96f1a47803d26fd5215a2f6
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-24 12:05:40 +0900

    [level 1] Title: 인기있는 아이스크림, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+-- SELECT flavor
+--   from first_half
+-- order by total_order desc,shipment_id asc
+
+-- select flavor
+-- from first_half
+-- order by total_order desc, shipment_id asc;
+
+-- 26.6.24
+select  flavor
+from first_half
+order by total_order desc, shipment_id;
