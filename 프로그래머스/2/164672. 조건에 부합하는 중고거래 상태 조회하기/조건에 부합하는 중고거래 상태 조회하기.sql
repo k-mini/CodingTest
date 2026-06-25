c173d91e57e10cef9a96eaa554b69d7c295eca7b
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-25 23:08:04 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,12 +1,11 @@
 -- 코드를 입력하세요
--- 26.6.25
 SELECT board_id, writer_id,
        title,
        price,
-       case `status` when 'SALE' then '판매중'
+       case status when 'SALE' then '판매중'
        when 'RESERVED' then '예약중'
        when 'DONE' then '거래완료'
-       end as `status`
+       end as status
   from used_goods_board
-  where date_format(created_date, '%Y-%m-%d') = '2022-10-05'
+  where to_char(created_date, 'YYYY-MM-DD') = '2022-10-05'
   order by board_id desc;
