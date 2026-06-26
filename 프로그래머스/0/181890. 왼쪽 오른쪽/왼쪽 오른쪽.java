01fea5321fd334e60bf486fbb8c5d895fbf7f7de
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-26 22:30:07 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,25 +1,38 @@
 import java.util.*;
 import java.util.stream.*;
 class Solution {
-    public String[] solution(String[] str_list) {
-        String[] answer = {};
-        int idx = -1;
-        for (int i=0;  i < str_list.length; i++) {
-            if (str_list[i].equals("l") ) {
-                idx = i;
-                answer =IntStream.range(0,idx).mapToObj(j -> str_list[j])
-                                        .toArray(String[]::new);
-                break;
-            }
+//     public String[] solution(String[] str_list) {
+//         String[] answer = {};
+//         int idx = -1;
+//         for (int i=0;  i < str_list.length; i++) {
+//             if (str_list[i].equals("l") ) {
+//                 idx = i;
+//                 answer =IntStream.range(0,idx).mapToObj(j -> str_list[j])
+//                                         .toArray(String[]::new);
+//                 break;
+//             }
             
-            if ( str_list[i].equals("r")) {
-                idx = i;
-                answer =IntStream.range(idx+1 , str_list.length).mapToObj(j -> str_list[j])
-                                        .toArray(String[]::new);
-                break;
+//             if ( str_list[i].equals("r")) {
+//                 idx = i;
+//                 answer =IntStream.range(idx+1 , str_list.length).mapToObj(j -> str_list[j])
+//                                         .toArray(String[]::new);
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+    
+    public String[] solution(String[] str_list) {
+         for (int i = 0; i < str_list.length; i++) {
+
+            if ("l".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if ("r".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        
-        return answer;
+
+        return new String[0];
     }
 }
