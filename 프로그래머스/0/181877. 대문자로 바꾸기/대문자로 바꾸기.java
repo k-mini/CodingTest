005bf22511ec56d1af6bb6f03e7c982ea8b5a645
KwangMin Cho <83693386+k-mini@users.noreply.github.com>
2026-06-29 17:10:09 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        return myString.toUpperCase();
+    }
+}
